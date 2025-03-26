ac8ab275c395d83b2ff5cd7c676d30f1903d9856
marha-hwang <75074235+marha-hwang@users.noreply.github.com>
2025-03-26 22:52:59 +0900

    [Silver IV] Title: 큐, Time: 20 ms, Memory: 69508 KB -BaekjoonHub

--- Swift/백준/Silver/10845. 큐/큐.swift
+++ Swift/백준/Silver/10845. 큐/큐.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 Q18258().main()
 
 class Q18258{
@@ -13,30 +11,61 @@ class Q18258{
     }
     
     func solution(arr:[String]){
-        var que:[Int] = []
+        var que = Queue()
         for str in arr{
             let split = str.split(separator: " ")
             switch split[0] {
             case "push":
-                que.append(Int(split[1])!)
+                que.add(n:Int(split[1])!)
             case "pop":
-                if que.isEmpty{
-                    print(-1)
-                }
-                else{
-                    print(que.removeFirst())
-                }
+                print(que.pop() ?? -1)
             case "size":
-                print(que.count)
+                print(que.size())
             case "empty":
-                print(que.count == 0 ? 1:0)
+                print(que.size() == 0 ? 1:0)
             case "front":
-                print(que.first ?? -1)
+                print(que.first() ?? -1)
             case "back":
-                print(que.last ?? -1)
+                print(que.last() ?? -1)
             default:
                 print()
             }
         }
+    }
+}
+
+
+struct Queue {
+    var queue: [Int] = []
+    var head = 0
+    
+    func size()->Int{
+        return queue.count - head
+    }
+    
+    func empty()->Bool{
+        return size() > 0 ? false:true
+    }
+    
+    func first()->Int?{
+        return empty() ? nil:queue[head]
+    }
+    
+    func last()->Int?{
+        return empty() ? nil:queue.last
+    }
+    
+    mutating func add(n:Int){
+        queue.append(n)
+    }
+    
+    mutating func pop() -> Int? {
+        guard !empty() else{
+            return nil
+        }
+        
+        let result = queue[head]
+        head+=1
+        return result
     }
 }
